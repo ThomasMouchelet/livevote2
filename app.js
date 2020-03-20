81958d036cd4f0e8fda11b58c66b59b705994b53
thomas mouchelet <mouchelet.thomas@gmail.com>
2020-03-20 14:59:29 +0100

    add socket io

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const app = express()
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
     res.sendFile(`${__dirname}/index.html`)
 })
 
-app.listen(3000)
+io.on('connection', function (socket) {
+    console.log("connect")
+})
+
+http.listen(3000)
